7e9e784503089c40f4255572f33c419dea17f357
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-10 12:59:24 -0300

    created component with properties.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import React from 'react';
 
 import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
 
 const tag = <strong>Olá React!!!</strong>
 
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro titulo="Situação do Aluno" aluno="Pedro" nota={9.3}/>
+        <ComParametro titulo="Situação do Aluno" aluno="Maria" nota={9.9}/>
     </div>,
     document.getElementById("root")
 );
