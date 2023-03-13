f76d1db22af47ae6ced3665ab658fe448633cf7b
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-13 09:59:32 -0300

    created table component, and creating table columns and rows with map.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 
+import TabelaProdutos from "./components/repeticao/TabelaProdutos";
 import ListaAlunos from "./components/repeticao/ListaAlunos";
 import Familia from "./components/basicos/Familia";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
@@ -15,6 +16,9 @@ export default () => {
     <div className="App">
       <h1>Fundamentos React</h1>
       <div className="Cards">
+        <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
+            <TabelaProdutos />
+        </Card>
         <Card titulo="#06 - Repetição">
             <ListaAlunos />
         </Card>
