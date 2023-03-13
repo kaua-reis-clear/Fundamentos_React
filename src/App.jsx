ebc0c6407ff3cdd181d1214363aa30e8e75a863b
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-13 08:40:38 -0300

    components adds and organizeds into a card component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import Aleatorio from './components/basicos/Aleatorio';
-import Primeiro from './components/basicos/Primeiro';
-import ComParametro from './components/basicos/ComParametro';
-import Fragmento from './components/basicos/Fragmento';
+import Card from "./components/layout/Card";
+import Aleatorio from "./components/basicos/Aleatorio";
+import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
 
 export default () => {
-    return (
-        <div id="app">
-            <h1>Fundamentos React</h1>
-            <Aleatorio min={1} max={60}/>
-            <Fragmento/>
-            <ComParametro 
-            titulo="Situação do Aluno"
-            aluno="Pedro Silva"
-            nota={9.3}/>
-            <Primeiro></Primeiro>
-        </div>
-    )
-}
+  return (
+    <div id="app">
+      <h1>Fundamentos React</h1>
+      <Card titulo="#04 - Desafio Aleatório">
+        <Aleatorio min={1} max={60} />
+      </Card>
+
+      <Card titulo="#03 - Fragmento">
+        <Fragmento />
+      </Card>
+
+      <Card titulo="#02 - Com Parâmetro">
+        <ComParametro
+          titulo="Situação do Aluno"
+          aluno="Pedro Silva"
+          nota={9.3}
+        />
+      </Card>
+
+      <Card titulo="#01 - Primeiro Componente">
+        <Primeiro></Primeiro>
+      </Card>
+    </div>
+  );
+};
---
+++ src/components/layout/Card.jsx
@@ -0,0 +1,11 @@
+import "./Card.css";
+import React from "react";
+
+export default (props) => {
+  return (
+    <div className="Card">
+      <div className="Title">Conteúdo</div>
+      <div className="Content">{props.children}</div>
+    </div>
+  );
+};
