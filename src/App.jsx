81f3a9a23a73190b35992cadc6b5fde114f35c63
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-13 10:26:38 -0300

    using conditionals to render a component with an info or another.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import React from "react";
 
+import UsuarioInfo from "./components/condicional/UsuarioInfo";
+import ParOuImpar from "./components/condicional/ParOuImpar";
 import TabelaProdutos from "./components/repeticao/TabelaProdutos";
 import ListaAlunos from "./components/repeticao/ListaAlunos";
 import Familia from "./components/basicos/Familia";
@@ -16,17 +18,25 @@ export default () => {
     <div className="App">
       <h1>Fundamentos React</h1>
       <div className="Cards">
+        <Card titulo="#08 - Rederização Condicional" color="#3A9AD9">
+          <ParOuImpar numero={20} />
+          <UsuarioInfo usuario={{nome: 'Fernando'}} />
+          <UsuarioInfo />
+        </Card>
+
         <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
-            <TabelaProdutos />
+          <TabelaProdutos />
         </Card>
+
         <Card titulo="#06 - Repetição">
-            <ListaAlunos />
+          <ListaAlunos />
         </Card>
+
         <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
           <Familia sobrenome="Silva">
-            <FamiliaMembro nome="Pedro"/>
-            <FamiliaMembro nome="Ana"/>
-            <FamiliaMembro nome="Gustavo"/>
+            <FamiliaMembro nome="Pedro" />
+            <FamiliaMembro nome="Ana" />
+            <FamiliaMembro nome="Gustavo" />
           </Familia>
         </Card>
 
