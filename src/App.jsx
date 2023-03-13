6cc3173e0f2db44ed5f66dfea149dbc2b5d68836
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-13 09:08:05 -0300

    created a child component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 
+import Familia from "./components/basicos/Familia";
 import Card from "./components/layout/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from "./components/basicos/Primeiro";
@@ -12,6 +13,10 @@ export default () => {
     <div className="App">
       <h1>Fundamentos React</h1>
       <div className="Cards">
+        <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+          <Familia sobrenome="Ferreira"></Familia>
+        </Card>
+
         <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
           <Aleatorio min={1} max={60} />
         </Card>
