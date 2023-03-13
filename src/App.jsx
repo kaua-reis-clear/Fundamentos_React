7135be1042060f47801f3bdeacf6f73f2b393491
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-13 12:23:49 -0300

    created component to draft a customized amount of random numbers.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 
+import Mega from "./components/mega/Mega";
 import Contador from "./components/contador/Contador";
 import Input from "./components/formulario/Input";
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
@@ -22,6 +23,10 @@ export default () => {
     <div className="App">
       <h1>Fundamentos React</h1>
       <div className="Cards">
+        <Card titulo="#12 - Contador" color="#89006E">
+          <Mega></Mega>
+        </Card>
+
         <Card titulo="#12 - Contador" color="#424242">
           <Contador numeroInicial={10}></Contador>
         </Card>
