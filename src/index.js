b91ffd4953d43cf625e174a71b19c9b44a4e6f8c
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-13 08:11:51 -0300

    using empty tag to nest tags in a component without a parent.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
+import Fragmento from './components/basicos/Fragmento';
 
 const tag = <strong>Olá React!!!</strong>
 
